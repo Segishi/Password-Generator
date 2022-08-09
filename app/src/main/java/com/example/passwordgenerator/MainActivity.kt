9d8eb0958cdfdd91fadbd07027299484983f67b1
Von Taylor <taylorvon525@gmail.com>
2022-08-08 22:50:57 -0700

    sixth commit
    mods: fixed password length input; next: switches and copy button

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -19,10 +19,10 @@ class MainActivity : AppCompatActivity() {
         val sampleUpperAlphabets:Array<Char> = arrayOf('A','B','C','D','E','F','G','H','I','J',
             'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')    // sample array of uppercase alphabets
         val sampleLowerAlphabets:Array<Char> = arrayOf('a','b','c','d','e','f','g','h','i','j','k',
-        'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')    // sample array of lowercase alphabets
+            'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')    // sample array of lowercase alphabets
         val sampleSymbols:Array<Char> = arrayOf('!','@','#','$','%','&','?')    // sample array of special characters
         val sampleNums:Array<Int> = arrayOf(1,2,3,4,5,6,7,8,9)      // sample array of numbers
-        val passLength:Int = 12     // the length desired length of the password
+        var passLength:Int = 10     // the length desired length of the password
         var password:String = ""    // the password that will be created/generated
 
         var upperAlphabets:Array<Char> = sampleUpperAlphabets   // uppercase alphabets array to be used in random generator
@@ -30,8 +30,48 @@ class MainActivity : AppCompatActivity() {
         var symbols:Array<Char> = sampleSymbols     // symbol array to be used in random generator
         var nums:Array<Int> = sampleNums    // nums array to be used in random generator
 
+        // hide the password with asterisks when checked, and reveal it when unchecked
+        hidePasswordSwitch.setOnClickListener {
+
+        }
+
+        // allow uppercase letters when checked, and disable when unchecked
+        upperCaseSwitch.setOnClickListener {
+            if(upperCaseSwitch.isChecked){
+                upperAlphabets = sampleUpperAlphabets
+            } else {
+                upperAlphabets = arrayOf()
+            }
+        }
+
+        // allow lowercase letters when checked, and disable when unchecked
+        lowerCaseSwitch.setOnClickListener {
+
+        }
+
+        // allow digits when checked, and disable when unchecked
+        digitsSwitch.setOnClickListener {
+
+        }
+
+        // allow symbols when checked, and disable when unchecked
+        symbolsSwitch.setOnClickListener {
+
+        }
+
+        // set the length of the password based on the inputted number in "passLengthTextBox" when the "SET" button is pressed
+        setButton.setOnClickListener {
+            if(Integer.parseInt(passwordLengthTextBox.text.toString()) in 1..12) {
+                passLength = Integer.parseInt(passwordLengthTextBox.text.toString())
+                passwordLengthTextBox.text.clear()
+            } else {
+                passwordTextBox.text = "Invalid Number!"
+                passwordLengthTextBox.text.clear()
+            }
+        }
+
+        // generate the random password using the 'random' package and arrays when the "GENERATE" button is pressed
         generateButton.setOnClickListener {
-            // generate the random password using the 'random' package and arrays
             password = ""
             var i:Int = 0
             while(i < passLength) {
